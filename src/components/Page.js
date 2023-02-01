0b51e931dc470c1e3bf7a81517d8594cb0a28eb9
r0w4n <6553343+r0w4n@users.noreply.github.com>
2023-02-01 20:08:22 +0000

    added footer

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,6 +1,10 @@
-import { ThemeProvider, createTheme, CssBaseline, Container } from "@mui/material/";
+import { BottomNavigation, BottomNavigationAction, ThemeProvider, createTheme, CssBaseline, Container } from "@mui/material/";
 import PropTypes from "prop-types";
 import CookieConsent from "react-cookie-consent";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
+import BugReportIcon from "@mui/icons-material/BugReport";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
 
 import AppBar from "./AppBar";
 
@@ -32,6 +36,24 @@ function Page(props) {
             <Container maxWidth="lg">
                 <AppBar />
                 {props.children}
+                <BottomNavigation showLabels>
+                    <BottomNavigationAction label="Github" icon={<GitHubIcon />} href="https://github.com/r0w4n/cs-pension" />
+                    <BottomNavigationAction
+                        label="Donate"
+                        icon={<VolunteerActivismIcon />}
+                        href="https://www.paypal.com/donate/?hosted_button_id=37J8F7GCENZVA    "
+                    />
+                    <BottomNavigationAction
+                        label="Report Bug"
+                        icon={<BugReportIcon />}
+                        href="https://github.com/r0w4n/cs-pension/issues/new?assignees=&labels=&template=bug_report.md&title="
+                    />
+                    <BottomNavigationAction
+                        label="Suggestions"
+                        icon={<LightbulbIcon />}
+                        href="https://github.com/r0w4n/cs-pension/issues/new?assignees=&labels=&template=feature_request.md&title="
+                    />
+                </BottomNavigation>
             </Container>
             <CookieConsent>This website uses cookies for Google Analytics</CookieConsent>
         </ThemeProvider>
