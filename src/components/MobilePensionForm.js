ff89a8d9159d4dce9148c78bf4f5c2ca11323e6b
r0w4n <6553343+r0w4n@users.noreply.github.com>
2026-06-18 21:37:08 +0100

    Bump deps, update links and UI label
    Upgrade various runtime and dev dependencies (emotion, MUI, React, testing and ESLint/prettier packages). Replace external info links in PensionForm and MobilePensionForm with updated memberhub URLs (including EPA link). Fix BottomNavigation label capitalization to "GitHub". (yarn.lock updated by dependency changes.)

--- src/components/MobilePensionForm.js
+++ src/components/MobilePensionForm.js
@@ -87,10 +87,7 @@ class MobilePensionForm extends React.Component {
                     <Grid width="100%">
                         <InputLabel htmlFor="currentPensionPot">
                             Accrued Pension To Date
-                            <a
-                                href="https://www.civilservicepensionscheme.org.uk/your-pension/yearly-pension-update/annual-benefit-statement-abs/"
-                                target="_blank"
-                                rel="noreferrer">
+                            <a href="https://www.civilservicepensionscheme.org.uk/memberhub/KBarticle/?id=KA-01366" target="_blank" rel="noreferrer">
                                 <InfoTwoToneIcon />
                             </a>
                         </InputLabel>
@@ -122,7 +119,7 @@ class MobilePensionForm extends React.Component {
                         <InputLabel htmlFor="monthlyAddedPensionPayment">
                             Monthly Added Pension Payments
                             <a
-                                href="https://www.civilservicepensionscheme.org.uk/your-pension/managing-your-pension/increase-your-pension/added-pension/"
+                                href="https://www.civilservicepensionscheme.org.uk/memberhub/your-pension/managing-your-pension/added-pension/"
                                 target="_blank"
                                 rel="noreferrer">
                                 <InfoTwoToneIcon />
@@ -158,7 +155,7 @@ class MobilePensionForm extends React.Component {
                             EPA
                             <a
                                 // eslint-disable-next-line max-len
-                                href="https://www.civilservicepensionscheme.org.uk/planning-for-retirement/what-are-my-options/early-retirement/epa-effective-pension-age/"
+                                href="https://www.civilservicepensionscheme.org.uk/memberhub/planning-for-retirement/what-are-my-options/early-retirement/epa/"
                                 target="_blank"
                                 rel="noreferrer">
                                 <InfoTwoToneIcon />
